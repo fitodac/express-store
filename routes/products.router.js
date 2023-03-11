3bd3eb7c013dbb327ac71d05efb093e13c081265
fitodac <fitodac@gmail.com>
2023-03-11 14:37:03 -0300

    Change index.js by server and app files

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,26 +1,32 @@
 const express = require('express')
-const faker = require('faker')
+const { faker } = require('@faker-js/faker')
 const router = express.Router()
 
 router.get('/', (req,res) => {
 	const products = []
 	const{size} = req.query
 	const limit = size || 10
+
+	console.log(faker.commerce)
+
 	for(let index = 0; index < limit; index++){
 		products.push({
-			name: faker.commerce.productName(),
-			price: parseInt(faker.commerce.price(),10),
-			image: faker.image.imageUrl()
+			name: faker.commerce.product(),
+			price: parseInt(faker.commerce.price(), 10),
+			image: faker.image.food()
 		})
 	}
+
 	res.json(products)
 })
 
-router.get('/filter', (req,res) => {
+
+router.get('/filter', (req, res) => {
 	res.send('Yo soy un filter')
 })
 
-router.get('/:id', (req,res) => {
+
+router.get('/:id', (req, res) => {
 	const { id } = req.params
 	res.json({
 		id,
@@ -29,7 +35,8 @@ router.get('/:id', (req,res) => {
 	})
 })
 
-router.post('/', (req,res) => {
+
+router.post('/', (req, res) => {
 	const body = req.body
 	res.json({
 		message: 'created',
@@ -37,7 +44,7 @@ router.post('/', (req,res) => {
 	})
 })
 
-router.patch('/:id', (req,res) => {
+router.patch('/:id', (req, res) => {
 	const { id } = req.params
 	const body = req.body
 	res.json({
@@ -47,7 +54,7 @@ router.patch('/:id', (req,res) => {
 	})
 })
 
-router.delete('/:id', (req,res) => {
+router.delete('/:id', (req, res) => {
 	const { id } = req.params
 	res.json({
 		id,
--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require('express')
 const routerApi = require('./routes')
 const app = express()
-const port = 3000
 
 app.use(express.json())
 
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
 	res.send('Hola mi server en express')
 })
 
-app.get('/nueva-ruta',(req,res) => {
+app.get('/nueva-ruta', (req, res) => {
 	res.send('Hola, soy una nueva ruta')
 })
 
 
 routerApi(app)
 
-app.listen(port, () => {
-	console.log('Mi port '+port)
-})
+module.exports = { app }
