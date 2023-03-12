d1d0b3db523cafdcc797d98197889d24a1924c10
fitodac <fitodac@gmail.com>
2023-03-12 02:24:46 -0300

    Validation middlewares

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const routerApi = require('./routes')
 const app = express()
+const { 
+	logErrors, 
+	errorHandler, 
+	boomErrorHandler 
+} = require('./middlewares/error.handler')
 
 app.use(express.json())
-
 
 app.get('/', (req, res) => {
 	res.send('Hola mi server en express')
@@ -15,5 +19,9 @@ app.get('/nueva-ruta', (req, res) => {
 
 
 routerApi(app)
+app.use(logErrors)
+app.use(boomErrorHandler)
+app.use(errorHandler)
+
 
 module.exports = app
--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,5 @@
 const { faker } = require('@faker-js/faker')
+const boom = require('@hapi/boom')
 
 class ProductsService
 {
@@ -15,7 +16,8 @@ class ProductsService
 				id: faker.datatype.uuid(),
 				name: faker.commerce.product(),
 				price: parseInt(faker.commerce.price(), 10),
-				image: faker.image.food()
+				image: faker.image.food(),
+				isBlock: faker.datatype.boolean()
 			})
 		}
 	}
@@ -32,29 +34,34 @@ class ProductsService
 
 	async find() {
 		return new Promise((resolve, reject) => {
-			setTimeout(() => resolve(this.products), 2000)
+			setTimeout(() => resolve(this.products), 1000)
 		})
 	}
 
 
 	async findOne(id) {
-		return this.products.find(item => item.id === id)
+		const product = this.products.find(item => item.id === id)
+		if( !product ){ throw boom.notFound('product not found') }
+		if( product.isBlock ){ throw boom.conflict('product is block') }
+		return product
 	}
 
 	async update(id, changes) {
 		const index = this.products.findIndex(item => item.id === id)
-		if( index === -1 ) throw new Error('product not found')
+		
+		if( index === -1 ) throw boom.notFound('product not found')
+		
 		const product = this.products[index]
 		this.products[index] = {
 			...product,
 			...changes
-		};
+		}
 		return this.products[index];
 	}
 
 	async delete(id) {
 		const index = this.products.findIndex(item => item.id === id)
-		if( index === -1 ) throw new Error('product not found')
+		if( index === -1 ) throw boom.notFound('product not found')
 		this.products.splice(index, 1)
 		return { id };
 	}
